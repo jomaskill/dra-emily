ddc680af7379091826c20761b5f8ab84fb4a07c3
Joao Marcos <jomasjoao@gmail.com>
2026-05-28 16:05:51 -0300

    add

--- config/clinic.php
+++ config/clinic.php
@@ -18,9 +18,9 @@ return [
 
     'address_zip' => env('CLINIC_ADDRESS_ZIP', '31255-750'),
 
-    'latitude' => env('CLINIC_LATITUDE', '-19.8897'),
+    'latitude' => env('CLINIC_LATITUDE', '-19.8611003'),
 
-    'longitude' => env('CLINIC_LONGITUDE', '-43.9612'),
+    'longitude' => env('CLINIC_LONGITUDE', '-43.9496221'),
 
     'domain' => env('CLINIC_DOMAIN', 'dra-emily-beatriz.com.br'),
 
